7c9d9a9e66c85fdcec9a37cc240c408cde99b88a
Pranav anilkumar Wani <pranavanilkumar.wani@hcl.com>
2024-03-15 11:17:41 +0530

    minor: using axios from cdn

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      // external: ['axios'], // Add 'axios' to the external array
+      external: ['axios'], // Add 'axios' to the external array
     },
   }
 })
